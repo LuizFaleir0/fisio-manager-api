3612c067c453cd3e23a3e30f290bc97bf762fadb
Luiz Faleiro <98827442+LuizFaleir0@users.noreply.github.com>
2024-06-03 15:25:32 -0300

    feat(config): configurar banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigGlobalModule } from './config/config.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+/**
+ * Módulo Principal
+ * @description Este módulo atua como ponto de entrada da aplicação
+ */
 @Module({
-  imports: [],
+  imports: [ConfigGlobalModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/config.module.ts
@@ -0,0 +1,38 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+/**
+ * Configurações de banco de dados
+ */
+const getTypeOrmModuleOptions = (
+  configService: ConfigService,
+): TypeOrmModuleOptions => ({
+  type: 'postgres',
+  host: configService.get<string>('DB_HOST'),
+  port: +configService.get<string>('DB_PORT'),
+  username: configService.get<string>('DB_USERNAME'),
+  password: configService.get<string>('DB_PASSWORD'),
+  database: configService.get<string>('DB_NAME'),
+});
+
+/**
+ * Módulo de Configuração
+ * @description Esse módulo é responsável pela configuração global do projeto.
+ */
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [
+        ConfigModule.forRoot({
+          // Configurando variáveis de ambiente
+          envFilePath: ['.env.development.local'],
+        }),
+      ],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        getTypeOrmModuleOptions(configService),
+    }),
+  ],
+})
+export class ConfigGlobalModule {}
